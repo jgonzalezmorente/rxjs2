3f987b2cfdb2d100136bf3e1efcb0d0691794849
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-11-18 22:30:57 +0100

    Operadores de aplanamiento

---
+++ src/transformacion/03-switchMap.ts
@@ -0,0 +1,53 @@
+import { Observable, debounceTime, fromEvent, map, mergeAll, mergeMap, switchMap } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { GithubUser } from '../interfaces/github-user.interface';
+import { GithubUsersResp } from '../interfaces/github-users.interface';
+
+
+// Referencias
+const body = document.querySelector( 'body' );
+const textInput = document.createElement( 'input' );
+const orderList = document.createElement( 'ol' );
+body.append( textInput, orderList );
+
+// Helpers
+const mostrarUsuarios = ( usuarios: GithubUser[] ) => {
+    orderList.innerHTML = '';
+    console.log( usuarios );
+
+    for( const usuario of usuarios ) {
+        const li = document.createElement('li');
+        li.classList.add('github-user');        
+        const img = document.createElement('img');
+        img.src = usuario.avatar_url;
+
+        const anchor = document.createElement('a');
+        anchor.href = usuario.html_url;
+        anchor.text = 'Ver página';
+        anchor.target = '_blank'
+        
+        li.append( img );
+        li.append( usuario.login + ' ' );
+        li.append( anchor );
+        orderList.append( li );
+    }
+
+}
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup' );
+
+input$.pipe(
+    debounceTime<KeyboardEvent>( 500 ),
+    map<KeyboardEvent, string>( ( { target } ) => target['value'] ),
+    mergeMap<string, Observable<GithubUsersResp>>( texto => ajax.getJSON( `https://api.github.com/search/users?q=${ texto }` ) ),    
+    map<GithubUsersResp, GithubUser[]>( resp => resp.items ),    
+);//.subscribe( mostrarUsuarios );
+
+
+const url = 'https://httpbin.org/delay/1?arg=';
+
+input$.pipe(
+    map( ( { target } ) => target[ 'value' ] ),
+    switchMap( texto => ajax.getJSON( url + texto ) )
+).subscribe( console.log );
